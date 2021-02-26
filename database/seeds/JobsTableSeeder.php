2adb8e409de6ce5a08ab518e1d6594bb6ca86df6
JethroOlwen <jeth.olwen@gmail.com>
2021-02-26 15:57:26 +0700

    part 59 - created user and job fake data

---
+++ database/seeds/JobsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Job;
+
+class JobsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Job::class,20)->create();
+    }
+}
